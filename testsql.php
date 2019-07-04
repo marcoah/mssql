48199d73eb0fe8ac50fbc8fceb3ad1cebb92f061
Marco Hernandez <marcoah@gmail.com>
2019-07-04 18:44:41 -0400

    first commit

---
+++ testsql.php
@@ -0,0 +1,29 @@
+<?php
+
+$serverName = "localhost\SQLExpress";
+$connectionOptions = array( "Database" => "geologia", "Uid" => "sa", "PWD" => "master" );
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+//Select Query
+$tsql= "SELECT @@Version as SQL_VERSION";
+//Executes the query
+$getResults= sqlsrv_query($conn, $tsql);
+//Error handling
+if ($getResults == FALSE) die(FormatErrors(sqlsrv_errors()));
+   ?>
+    <h1> Results : </h1>
+<?php
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo ($row['SQL_VERSION']); echo ("<br/>");
+    }
+    sqlsrv_free_stmt($getResults);
+    function FormatErrors( $errors ) {
+        /* Display errors. */
+        echo "Error information: <br/>";
+        foreach ( $errors as $error ) {
+            echo "SQLSTATE: ".$error['SQLSTATE']."<br/>";
+            echo "Code: ".$error['code']."<br/>";
+            echo "Message: ".$error['message']."<br/>";
+        }
+    }
+    ?>
