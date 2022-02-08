5fadb4363d564635c21ba18d94747d1e26d44f37
Marco Hernandez <marcoah@outlook.com>
2022-02-07 23:34:37 -0400

    Ajustes a las carpetas

--- adventureworks_demo.php
+++ adventureworks_demo.php
@@ -8,7 +8,7 @@
                    
 <?php
 
-    require('../vendor/autoload.php');
+    require('vendor/autoload.php');
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
--- test2.php
+++ test2.php
@@ -1,5 +1,5 @@
 <?php 
-    require('../vendor/autoload.php');
+    require('vendor/autoload.php');
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
@@ -17,7 +17,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
         <title>Lista de Productos</title>
     </head>
     <body>
@@ -64,6 +64,6 @@
             </table>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
